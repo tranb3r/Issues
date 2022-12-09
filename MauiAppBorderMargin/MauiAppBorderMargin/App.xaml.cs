6914e28e5e3fe9c3a1e5167f0671c723041cbf2f
tranb3r <tranb3r@gmail.com>
2022-12-09 14:48:48 +0100

    add border margin repro

---
+++ MauiAppBorderMargin/MauiAppBorderMargin/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppBorderMargin;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiAppBorderMargin/MauiAppBorderMargin/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBorderMargin;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
