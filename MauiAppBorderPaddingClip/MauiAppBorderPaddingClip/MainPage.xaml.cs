e089a95b3ca89bc68e9711ae21ea579ddba8dcb2
tranb3r <tranb3r@gmail.com>
2023-01-03 20:54:38 +0100

    add repro for ios border clipping issue

---
+++ MauiAppBorderPaddingClip/MauiAppBorderPaddingClip/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppBorderPaddingClip;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
+
---
+++ MauiAppBorderPaddingClip/MauiAppBorderPaddingClip/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiAppBorderPaddingClip;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
