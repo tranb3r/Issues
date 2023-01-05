95080f839b57518dd9fe893c3d89ff034919a33a
tranb3r <tranb3r@gmail.com>
2023-01-05 15:29:34 +0100

    add repro for collection header resize ios issue

---
+++ MauiAppCollectionViewHeaderResize/MauiAppCollectionViewHeaderResize/ViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace MauiAppCollectionViewHeaderResize
+{
+    internal class ViewModel
+    {
+        public ObservableCollection<string> ItemListHeader { get; set; }
+        public ObservableCollection<string> ItemList { get; set; }
+
+        public Command AddCommand => new (Add);
+
+        public ViewModel()
+        {
+            ItemListHeader = new ObservableCollection<string>
+            {
+                "Paris",
+                "London"
+            };
+            ItemList = new ObservableCollection<string>
+            {
+                "Red",
+                "Blue",
+                "Green"
+            };
+        }
+
+        private void Add()
+        {
+            ItemListHeader.Add("Madrid");
+        }
+    }
+}
