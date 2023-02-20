f3550a59de61b94322a58602d1e5e134348abdf2
tranb3r <tranb3r@gmail.com>
2023-02-20 10:03:27 +0100

    add workaround for border margin issue, using custom frame

--- MauiAppBorderMargin/MauiAppBorderMargin/App.xaml.cs
+++ MauiAppBorderMargin/MauiAppBorderMargin/App.xaml.cs
@@ -1,4 +1,10 @@
-﻿namespace MauiAppBorderMargin;
+﻿#if ANDROID
+using Android.Graphics.Drawables;
+#endif
+using Microsoft.Maui.Controls.Handlers.Compatibility;
+using Microsoft.Maui.Platform;
+
+namespace MauiAppBorderMargin;
 
 public partial class App : Application
 {
@@ -7,5 +13,38 @@ public partial class App : Application
 		InitializeComponent();
 
 		MainPage = new MainPage();
-	}
+
+#if ANDROID
+        FrameRenderer.Mapper.AppendToMapping(CustomFrame.CornerRadiiProperty.PropertyName, (frameRenderer, frame) =>
+        {
+            if (frame is CustomFrame customFrame)
+            {
+                if (frameRenderer.Background is GradientDrawable backgroundGradient)
+                {
+                    var topLeftCorner = frameRenderer.Context.ToPixels(customFrame.CornerRadii.TopLeft);
+                    var topRightCorner = frameRenderer.Context.ToPixels(customFrame.CornerRadii.TopRight);
+                    var bottomLeftCorner = frameRenderer.Context.ToPixels(customFrame.CornerRadii.BottomLeft);
+                    var bottomRightCorner = frameRenderer.Context.ToPixels(customFrame.CornerRadii.BottomRight);
+
+                    var cornerRadii = new[]
+                    {
+                        topLeftCorner,
+                        topLeftCorner,
+
+                        topRightCorner,
+                        topRightCorner,
+
+                        bottomRightCorner,
+                        bottomRightCorner,
+
+                        bottomLeftCorner,
+                        bottomLeftCorner,
+                    };
+
+                    backgroundGradient.SetCornerRadii(cornerRadii);
+                }
+            }
+        });
+#endif
+    }
 }
---
+++ MauiAppBorderMargin/MauiAppBorderMargin/CustomFrame.cs
@@ -0,0 +1,13 @@
+﻿namespace MauiAppBorderMargin
+{
+    public class CustomFrame : Frame
+    {
+        public static readonly BindableProperty CornerRadiiProperty = BindableProperty.Create(nameof(CustomFrame), typeof(CornerRadius), typeof(CustomFrame));
+        
+        public CornerRadius CornerRadii
+        {
+            get => (CornerRadius)GetValue(CornerRadiiProperty);
+            set => SetValue(CornerRadiiProperty, value);
+        }
+    }
+}
