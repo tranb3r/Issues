ba88557bf5202869fa7258f0b047fe7a2591e605
tranb3r <tranb3r@gmail.com>
2023-06-27 17:13:28 +0200

    add issue for numeric keyboard

---
+++ MauiAppKeyboardNumeric/MauiAppKeyboardNumeric/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MauiAppKeyboardNumeric;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ MauiAppKeyboardNumeric/MauiAppKeyboardNumeric/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿namespace MauiAppKeyboardNumeric;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+    private async void Button_OnClicked(object sender, EventArgs e)
+    {
+        await DisplayPromptAsync("Question", "What's 5 + 5?", initialValue: "10", maxLength: 3, keyboard: Keyboard.Numeric);
+    }
+}
