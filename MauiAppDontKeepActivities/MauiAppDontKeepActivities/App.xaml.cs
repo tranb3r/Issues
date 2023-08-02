17e143db8b9cb60b96785ec187b12dea40ec6719
tranb3r <tranb3r@gmail.com>
2023-08-02 21:14:17 +0200

    add mauiAppDontKeepActivities repro

---
+++ MauiAppDontKeepActivities/MauiAppDontKeepActivities/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppDontKeepActivities
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+    }
+}
---
+++ MauiAppDontKeepActivities/MauiAppDontKeepActivities/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiAppDontKeepActivities
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
