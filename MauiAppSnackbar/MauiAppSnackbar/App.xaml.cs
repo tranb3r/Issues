c8a0d7109d43736f0e39c2e334554071a9d3d816
tranb3r <tranb3r@gmail.com>
2023-10-16 20:59:56 +0200

    add mauiappsnackbar repro

---
+++ MauiAppSnackbar/MauiAppSnackbar/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppSnackbar
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ MauiAppSnackbar/MauiAppSnackbar/MainPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using CommunityToolkit.Maui.Alerts;
+
+namespace MauiAppSnackbar
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            var toast = Snackbar.Make("Hello");
+
+            try
+            {
+                await toast.Show();
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Exception", ex.ToString(), "Cancel");
+            }
+        }
+    }
+}
