0add132963e730d2807a0f8be3aa652f63b60291
tranb3r <tranb3r@gmail.com>
2024-06-17 12:11:23 +0200

    update repro

--- MauiAppCollectionViewGroupingLeak/TestVirtualization.MauiApp/Views/Header.xaml.cs
+++ MauiAppCollectionViewGroupingLeak/TestVirtualization.MauiApp/Views/Header.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Maui.Controls;
+﻿using System;
+using System.Timers;
+using Microsoft.Maui.ApplicationModel;
+using Microsoft.Maui.Controls;
 using TestVirtualization.MauiApp.Models;
 
 namespace TestVirtualization.MauiApp.Views
@@ -8,13 +11,31 @@ namespace TestVirtualization.MauiApp.Views
 		public Header ()
 		{
 			InitializeComponent ();
-		}
+
+            StartMemoryTimer();
+        }
 
         protected override void OnBindingContextChanged()
         {
             base.OnBindingContextChanged();
 
 			Counters.Instance.Reset();
+        }
+
+        private void StartMemoryTimer()
+        {
+            var timer = new Timer(1000);
+            timer.Elapsed += (_, _) =>
+            {
+                var memory = GC.GetTotalMemory(false) / 1024d / 1024d;
+                MainThread.BeginInvokeOnMainThread(() => MemLabelRef.Text = $"Mem: {memory:F1} MB");
+            };
+            timer.Start();
+        }
+
+        private void ButtonGC_OnClicked(object sender, EventArgs e)
+        {
+            GC.Collect();
         }
     }
 }
