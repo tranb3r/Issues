a8258c365de90fe79197f97732e356559d9671c1
tranb3r <tranb3r@gmail.com>
2024-06-25 16:21:23 +0200

    add mauiappsegfault repro

---
+++ MauiAppSegfault/MauiAppSegfault/MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using HtmlAgilityPack;
+using System;
+
+namespace MauiAppSegfault
+{
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_OnClicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var node = default(HtmlNode);
+                var link = node.GetAttributeValue("href", null);
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }
+}
---
+++ MauiAppSegfault/MauiAppSegfault/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Maui.Controls.Hosting;
+using Microsoft.Maui.Hosting;
+using Sentry;
+
+namespace MauiAppSegfault
+{
+    public static class MauiProgram
+    {
+        private const string Dsn = "INSERT YOUR DSN";
+
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>();
+
+            SentrySdk.Init(Dsn);
+
+            return builder.Build();
+        }
+    }
+}
