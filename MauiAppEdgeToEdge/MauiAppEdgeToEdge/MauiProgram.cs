6997283b84dc9b0f38d16ab246ce9c4aff4c147d
redth <jondick@gmail.com>
2024-10-04 10:47:25 -0400

    Edge to Edge on Modals for .NET 9

--- MauiAppEdgeToEdge/MauiAppEdgeToEdge/MauiProgram.cs
+++ MauiAppEdgeToEdge/MauiAppEdgeToEdge/MauiProgram.cs
@@ -1,4 +1,9 @@
-﻿namespace MauiAppEdgeToEdge
+﻿using AndroidX.Activity;
+using AndroidX.Fragment.App;
+using Microsoft.Maui.LifecycleEvents;
+using Microsoft.Maui.Platform;
+
+namespace MauiAppEdgeToEdge
 {
     public static class MauiProgram
     {
@@ -6,9 +11,47 @@
         {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>();
+                .UseMauiApp<App>()
+				.ConfigureLifecycleEvents(lifecycleBuilder =>
+				{
+#if ANDROID
+					lifecycleBuilder.AddAndroid(androidLifecycleBuilder =>
+					{
+						androidLifecycleBuilder.OnCreate((activity, savedInstance) =>
+						{
+							if (activity is ComponentActivity componentActivity)
+							{
+								// Enable Edge to Edge for the activity
+								EdgeToEdge.Enable(componentActivity);
+
+								// Also wire up a fragment lifecycle callback so we can enable edge to edge on fragments
+								componentActivity.GetFragmentManager()?.RegisterFragmentLifecycleCallbacks(new MyFragmentLifecycleCallbacks((fragmentManager, fragment) =>
+								{
+									// Modals in MAUI in NET9 use DialogFragment
+									if (fragment is DialogFragment dialogFragment)
+									{
+										// Edge to Edge on the fragment's window
+										dialogFragment.Dialog!.Window!.EnableEdgeToEdge(dialogFragment.Dialog!.Window!.DecorView!.Resources!);
+									}
+								}), false);
+							}
+						});
+					});
+#endif
+				});
 
             return builder.Build();
         }
     }
 }
+
+#if ANDROID
+public class MyFragmentLifecycleCallbacks(Action<FragmentManager, Fragment> onFragmentStarted) : FragmentManager.FragmentLifecycleCallbacks
+{
+	public override void OnFragmentStarted(FragmentManager fm, Fragment f)
+	{
+		onFragmentStarted?.Invoke(fm, f);
+		base.OnFragmentStarted(fm, f);
+	}
+}
+#endif
--- MauiAppEdgeToEdge/MauiAppEdgeToEdge/Platforms/Android/MainActivity.cs
+++ MauiAppEdgeToEdge/MauiAppEdgeToEdge/Platforms/Android/MainActivity.cs
@@ -9,15 +9,5 @@ namespace MauiAppEdgeToEdge
     [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
-        protected override void OnCreate(Bundle? savedInstanceState)
-        {
-            base.OnCreate(savedInstanceState);
-
-            EdgeToEdge.Enable(this);
-
-            //WindowCompat.SetDecorFitsSystemWindows(Window, false);
-            //Window.SetStatusBarColor(global::Android.Graphics.Color.Transparent);
-            //Window.SetNavigationBarColor(global::Android.Graphics.Color.Transparent);
-        }
     }
 }
