a189aaf4f043e566f1594420c17d8772288d549d
tranb3r <tranb3r@gmail.com>
2025-03-14 21:20:30 +0100

    add MauiAppCollectionViewIsVisibleRegression

---
+++ MauiAppCollectionViewIsVisibleRegression/MauiAppCollectionViewIsVisibleRegression/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Maui.Controls;
+
+namespace MauiAppCollectionViewIsVisibleRegression
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override async void OnAppearing()
+        {
+            CollectionViewRef.ItemsSource = new ObservableCollection<string>(Enumerable.Range(0, 1000).Select(i => $"Item {i}"));
+            await Task.Delay(10000);
+            CollectionViewRef.IsVisible = true;
+        }
+    }
+}
